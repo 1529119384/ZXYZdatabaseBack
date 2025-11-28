517d84b06568a0014dec19f7631151db12a719df
2403_89608231 <zzc1529119384@163.com>
2025-11-28 21:27:20 +0800

    新增文件异常统一处理

--- src/main/java/uno/acloud/exception/GlobalExceptionHandler.java
+++ src/main/java/uno/acloud/exception/GlobalExceptionHandler.java
@@ -1,17 +1,25 @@
 package uno.acloud.exception;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import uno.acloud.pojo.Result;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler (Exception.class)
-    public Result ex(Exception e) {
-        log.error("全局异常抛出");
-        e.printStackTrace();
+
+    /* 1. 业务异常：给用户看具体原因 */
+    @ExceptionHandler(FileUploadException.class)
+    public Result handleFileUpload(FileUploadException e) {
+        log.warn("文件上传业务异常：{}", e.getMessage());
+        return Result.error(e.getMessage());
+    }
+
+    /* 2. 兜底：其他所有异常 */
+    @ExceptionHandler(Exception.class)
+    public Result handleOther(Exception e) {
+        log.error("系统异常", e);
         return Result.error("操作失败，请联系acloud");
     }
 }
