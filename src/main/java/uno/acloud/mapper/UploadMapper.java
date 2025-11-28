5f5d806307233562c9c070aa53c09333562030b7
2403_89608231 <zzc1529119384@163.com>
2025-11-28 22:07:42 +0800

    修改了mapper层的配置

--- src/main/java/uno/acloud/mapper/UploadMapper.java
+++ src/main/java/uno/acloud/mapper/UploadMapper.java
@@ -3,9 +3,8 @@ package uno.acloud.mapper;
 import org.apache.ibatis.annotations.Insert;
 import uno.acloud.pojo.FileInfo;
 
-public class UploadMapper {
+public interface UploadMapper {
     @Insert("INSERT INTO file_info (file_type, file_name, category, file_size, file_url, store_path, user_id, parent_id, create_time, modify_time, deleted) " +
             "VALUES (#{fileType}, #{fileName}, #{category}, #{fileSize}, #{fileUrl}, #{storePath}, #{userId}, #{parentId}, #{createTime}, #{modifyTime}, #{deleted})")
-    public static void add(FileInfo fileInfo) {
-    }
+    void add(FileInfo fileInfo);
 }
