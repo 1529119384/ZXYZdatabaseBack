507589dca925ceb595c786b424aa218046a3ead3
2403_89608231 <zzc1529119384@163.com>
2025-11-30 23:40:38 +0800

    正常提交

--- src/main/java/uno/acloud/service/UploadService.java
+++ src/main/java/uno/acloud/service/UploadService.java
@@ -2,7 +2,9 @@ package uno.acloud.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.UnsupportedEncodingException;
+
 public interface UploadService {
-    String upload(MultipartFile file, Long parentId,Integer userId);
+    String upload(MultipartFile file, Long parentId,Integer userId) throws UnsupportedEncodingException;
 
 }
