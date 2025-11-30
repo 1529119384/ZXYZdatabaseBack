bb9a9e7f243c9238723bf7309c53caf76e44f02c
2403_89608231 <zzc1529119384@163.com>
2025-11-30 21:31:36 +0800

    伴随文件信息类修改而修改

--- src/main/java/uno/acloud/service/impl/UploadServiceImpl.java
+++ src/main/java/uno/acloud/service/impl/UploadServiceImpl.java
@@ -8,11 +8,13 @@ import org.springframework.web.multipart.MultipartFile;
 import uno.acloud.anno.Log;
 import uno.acloud.mapper.UploadMapper;
 import uno.acloud.pojo.FileInfo;
-import uno.acloud.pojo.Result;
 import uno.acloud.service.UploadService;
+import uno.acloud.utils.FileNameUtil;
+import uno.acloud.utils.FileTypeUtil;
 import uno.acloud.utils.OSSUploader;
 import uno.acloud.utils.UploadToLocal;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Slf4j
@@ -28,7 +30,7 @@ public class UploadServiceImpl implements UploadService {
 
     @Log
     @PostMapping("/upload")
-    public String upload(MultipartFile file, Long parentId,Integer userId) {
+    public String upload(MultipartFile file, Long parentId, Integer userId) {
         if (file == null || file.isEmpty()) {
             return null;
         }
@@ -45,18 +47,26 @@ public class UploadServiceImpl implements UploadService {
         FileInfo fileInfo = new FileInfo();
 
         fileInfo.setFileType(1);
-        fileInfo.setFileName(file.getOriginalFilename());
-        /*
-         * 文件小类没更新
-         * */
-        fileInfo.setCategory(0);
+
+        fileInfo.setUuidName(FileNameUtil.uuidName(file.getOriginalFilename()));
+        fileInfo.setOriginalName(file.getOriginalFilename());
+
+
+        int typeCode;
+        try {
+            typeCode = FileTypeUtil.classify(file.getInputStream(), file.getOriginalFilename());
+        } catch (IOException e) {
+            return null;
+        }
+        fileInfo.setCategory(typeCode);
+
         fileInfo.setFileSize(file.getSize());
         /*
-        * 文件路径没更新，前端还没适配
-        * */
+         * 文件路径没更新，前端还没适配
+         * */
         /*
-        * 上传文件没给元数据
-        * */
+         * 上传文件没给元数据
+         * */
         fileInfo.setStorePath("555");
         fileInfo.setFileUrl(url);
         fileInfo.setUserId(userId);
@@ -69,10 +79,9 @@ public class UploadServiceImpl implements UploadService {
         uploadMapper.add(fileInfo);
 
 
-
         return url;
 
-        // OSSUploader 成功返回 URL，失败抛 FileUploadException，由 GlobalExceptionHandler 转 Result
+//        OSSUploader 成功返回 URL，失败抛 FileUploadException，由 GlobalExceptionHandler 转 Result
 //        String url = ossUploader.upload(file);
 //        log.info("文件上传成功，访问地址：{}", url);
 //        return Result.success(url);
