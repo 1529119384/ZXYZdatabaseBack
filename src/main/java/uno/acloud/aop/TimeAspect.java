de2ec06df906d5abbe818a2e1510c53ba0541380
2403_89608231 <zzc1529119384@163.com>
2025-12-19 21:53:06 +0800

    正常提交，格式化代码

--- src/main/java/uno/acloud/aop/TimeAspect.java
+++ src/main/java/uno/acloud/aop/TimeAspect.java
@@ -6,16 +6,17 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
 @Slf4j
 @Aspect//aop 切面
 @Component
 public class TimeAspect {
     @Around("execution(* uno.acloud.service.*.*(..))")//切入点表达式
-public Object recordTime(ProceedingJoinPoint joinPoint) throws Throwable {
-    long start = System.currentTimeMillis();
-    Object result = joinPoint.proceed();
-    long end = System.currentTimeMillis();
-    log.info("方法{}耗时:{}ms",joinPoint.getSignature(), end - start);
-    return result;
-}
+    public Object recordTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long end = System.currentTimeMillis();
+        log.info("方法{}耗时:{}ms", joinPoint.getSignature(), end - start);
+        return result;
+    }
 }
