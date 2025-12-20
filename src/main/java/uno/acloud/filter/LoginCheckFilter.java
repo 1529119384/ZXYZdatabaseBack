48bb8d857e78d96760abb10726a0c833eecfc646
2403_89608231 <zzc1529119384@163.com>
2025-12-21 00:06:59 +0800

    弃用拦截器，全盘satoken托管

--- src/main/java/uno/acloud/filter/LoginCheckFilter.java
+++ src/main/java/uno/acloud/filter/LoginCheckFilter.java
@@ -1,18 +1,17 @@
 package uno.acloud.filter;
 
-import cn.dev33.satoken.stp.StpUtil;
-import com.alibaba.fastjson.JSONObject;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.StringUtils;
-import uno.acloud.pojo.Result;
-import uno.acloud.utils.JwtUtils;
 
 import java.io.IOException;
-
-@WebFilter("/*")
+/*
+*
+* 弃用
+*
+* */
+//@WebFilter("/*")
 @Slf4j
 public class LoginCheckFilter implements Filter {
 
@@ -32,28 +31,41 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        log.info("开始校验是否登录");
-        String jwt = request.getHeader("token");
-        if (!StringUtils.hasLength(jwt)) {
-            log.info("用户没有token，返回未登录页面");
-            Result error = Result.error("NO_LOGIN");
-            String noLogin = JSONObject.toJSONString(error);
-            servletResponse.getWriter().write(noLogin);
+// 只放掉 OPTIONS、login、register，其余交给 Sa-Token
+        if ("OPTIONS".equals(request.getMethod()) ||
+                requestURI.contains("login") ||
+                requestURI.contains("register")) {
+            filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
-        log.info("用户有令牌,开始校验令牌");
-        try {
-            JwtUtils.parseJWT(jwt);
-            Integer operateUserId = (Integer) StpUtil.getExtra(jwt,"userId");
-        } catch (Exception e) {
-            log.info("令牌校验失败,返回未登录页面");
-            Result error = Result.error("NO_LOGIN");
-            String noLogin = JSONObject.toJSONString(error);
-            servletResponse.getWriter().write(noLogin);
-            return;
-        }
-        log.info("令牌校验成功,放行");
+// 其他请求让 Sa-Token 自己拦
         filterChain.doFilter(servletRequest, servletResponse);
+
+//        log.info("开始校验是否登录");
+//        String jwt = request.getHeader("token");
+//
+//        if (!StringUtils.hasLength(jwt)) {
+//            log.info("用户没有token，返回未登录页面");
+//            Result error = Result.error("NO_LOGIN");
+//            String noLogin = JSONObject.toJSONString(error);
+//            servletResponse.getWriter().write(noLogin);
+//            return;
+//        }
+//        log.info("用户有令牌,开始校验令牌");
+//        try {
+////            JwtUtils.parseJWT(jwt);
+//            log.info(jwt);
+//            Integer operateUserId = Integer.valueOf(StpUtil.getExtra(jwt,"userId").toString());
+//            log.info("令牌校验成功,用户ID:{}",operateUserId);
+//        } catch (Exception e) {
+//            log.info("令牌校验失败,返回未登录页面,异常信息:{}",e.getMessage());
+//            Result error = Result.error("NO_LOGIN");
+//            String noLogin = JSONObject.toJSONString(error);
+//            servletResponse.getWriter().write(noLogin);
+//            return;
+//        }
+//        log.info("令牌校验成功,放行");
+//        filterChain.doFilter(servletRequest, servletResponse);
     }
 
 
