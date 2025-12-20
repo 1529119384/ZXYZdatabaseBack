d80cc7fef8fdceb6139bae5a8f51da80eceeb712
2403_89608231 <zzc1529119384@163.com>
2025-12-21 00:54:56 +0800

    增加接口鉴权

--- src/main/java/uno/acloud/controller/FileController.java
+++ src/main/java/uno/acloud/controller/FileController.java
@@ -1,5 +1,7 @@
 package uno.acloud.controller;
 
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.annotation.SaCheckRole;
 import cn.dev33.satoken.stp.StpUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -12,25 +14,24 @@ import uno.acloud.anno.Log;
 import uno.acloud.pojo.FileInfo;
 import uno.acloud.pojo.Result;
 import uno.acloud.service.FileService;
-import uno.acloud.utils.JwtUtils;
 import uno.acloud.utils.ServletUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @RestController
 public class FileController {
 
     @Autowired
-    private  FileService fileService;
+    private FileService fileService;
 
     @Log
     @PostMapping("/uploadFile")
+    @SaCheckPermission("admin")
     public Result upload(MultipartFile file, Long parentId, HttpServletRequest request) throws UnsupportedEncodingException {
         String jwt = ServletUtils.getToken(request);
-        int userId = Integer.parseInt(StpUtil.getExtra(jwt,"userId").toString());
+        int userId = Integer.parseInt(StpUtil.getExtra(jwt, "userId").toString());
 
         String url = fileService.upload(file, parentId, userId);
         if (url == null) {
@@ -42,9 +43,10 @@ public class FileController {
 
     @Log
     @PostMapping("/uploadFolder")
+    @SaCheckPermission("admin")
     public Result uploadFolder(String folderName, Long parentId, HttpServletRequest request) {
         String jwt = ServletUtils.getToken(request);
-        int userId = (Integer) StpUtil.getExtra(jwt,"userId");
+        int userId = (Integer) StpUtil.getExtra(jwt, "userId");
 
         Long folderId = fileService.uploadFolder(folderName, parentId, userId);
         if (folderId != null) {
@@ -58,14 +60,18 @@ public class FileController {
 
     @Log
     @GetMapping("/getFileList")
+    @SaCheckRole("user")
     public Result getFileList(Long parentId) {
         try {
             List<FileInfo> fileList = fileService.getFileListByParentId(parentId);
             log.info("获取文件夹{}下的文件列表成功", parentId);
+            log.info("文件列表:{}", fileList);
             return Result.success(fileList);
         } catch (Exception e) {
             log.error("获取文件夹{}下的文件列表失败", parentId, e);
             return Result.error("获取文件列表失败");
         }
     }
+
+
 }
